df49556d125953725e29c747ce4dc61ff8df51d3
tjconti12 <78386402+tjconti12@users.noreply.github.com>
2022-01-09 19:20:00 -0500

    Create AminInstance Class for Main Character

---
+++ Source/FirstProject/MainAminInstance.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainAminInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UMainAminInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		// Will get the owner of this animation instance
+		// Which pawn owns this AnimInstance
+		// Will initialize it if its a nullptr (will happen on first time)
+		Pawn = TryGetPawnOwner();
+	}
+}
+
+void UMainAminInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+		// Will get the owner of this animation instance
+		// Which pawn owns this AnimInstance
+		// Will initialize it if its a nullptr (will happen on first time)
+		Pawn = TryGetPawnOwner();
+	}
+
+	if (Pawn)
+	{
+		// Want to get the speed and if character is in the air
+		FVector Speed = Pawn->GetVelocity();
+		
+		// If Character is not jumping, we only want its X and Y speed (set the Z to 0)
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
+		
+		// Need to get magnitude of the vector, since Movement speed is just a float
+		// Size() gives us the magnitude of the vector as a float
+		MovementSpeed = LateralSpeed.Size();
+
+		// The Pawn MovementComponent has a function to see if the pawn is falling (in the air)
+		// Then sets the bIsInAir appropriately 
+		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
+
+
+	}
+}
---
+++ Source/FirstProject/MainAminInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAminInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_API UMainAminInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	// Similar to BeginPlay Function
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+	void UpdateAnimationProperties();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	bool bIsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	class APawn* Pawn;
+
+
+
+};
