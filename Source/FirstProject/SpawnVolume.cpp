5d372e3764f1a49571a9fbfdfa6da65ba2724428
tjconti12 <78386402+tjconti12@users.noreply.github.com>
2022-01-30 20:29:57 -0500

    Update Spawn Volume

--- Source/FirstProject/SpawnVolume.cpp
+++ Source/FirstProject/SpawnVolume.cpp
@@ -5,7 +5,8 @@
 #include "Components/BoxComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Engine/World.h"
-#include "Critter.h"
+#include "Enemy.h"
+#include "AIController.h"
 
 // Sets default values
 ASpawnVolume::ASpawnVolume()
@@ -22,6 +23,13 @@ void ASpawnVolume::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (Actor_1 && Actor_2 && Actor_3 && Actor_4)
+	{
+		SpawnArray.Add(Actor_1);
+		SpawnArray.Add(Actor_2);
+		SpawnArray.Add(Actor_3);
+		SpawnArray.Add(Actor_4);
+	}
 }
 
 // Called every frame
@@ -41,7 +49,7 @@ FVector ASpawnVolume::GetSpawnPoint()
 	return Point;
 }
 // THe _Implementation is to tell the engine that this is the implementation in C++ (due to the UFUNCTION macro BlueprintNativeEvent)
-void ASpawnVolume::SpawnOurPawn_Implementation(UClass* ToSpawn, const FVector& Location)
+void ASpawnVolume::SpawnOurActor_Implementation(UClass* ToSpawn, const FVector& Location)
 {
 	if (ToSpawn)
 	{
@@ -50,7 +58,33 @@ void ASpawnVolume::SpawnOurPawn_Implementation(UClass* ToSpawn, const FVector& L
 
 		if (World)
 		{
-			ACritter* CritterSpawned = World->SpawnActor<ACritter>(ToSpawn, Location, FRotator(0.f), SpawnParams);
+			AActor* Actor = World->SpawnActor<AActor>(ToSpawn, Location, FRotator(0.f), SpawnParams);
+
+			AEnemy* Enemy = Cast<AEnemy>(Actor);
+			if (Enemy)
+			{
+				Enemy->SpawnDefaultController();
+
+				AAIController* AICont = Cast<AAIController>(Enemy->GetController());
+				if (AICont)
+				{
+					Enemy->AIController = AICont;
+				}
+			}
 		}
+	}
+}
+
+TSubclassOf<AActor> ASpawnVolume::GetSpawnActor()
+{
+	if (SpawnArray.Num() > 0)
+	{
+		int32 Selection = FMath::RandRange(0, SpawnArray.Num() - 1);
+
+		return SpawnArray[Selection];
+	}
+	else 
+	{
+		return nullptr;
 	}
 }
